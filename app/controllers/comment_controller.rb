8b8d29f9d048d2b742e1f777e168c81902d35b35
kuroun <sk_roun@yahoo.com>
2013-12-11 16:18:59 +0700

    Finish vew and add comment

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -2,17 +2,16 @@ class CommentController < ApplicationController
   def index
       add_breadcrumb "Comment and Feedback", :comment_index_path,:title => "Back to Comment and Feedback Page"
       @comment = UserComment.new
-      respond_to do |format|
-        format.html # new.html.erb
-        format.json { render json: @comment }
-      end
+      @view_comment=UserComment.view
+      #@view_reply=UserReply.view(params[:id])
   end  
   def create
   @comment = UserComment.new(params[:user_comment])
+  @view_comment=UserComment.view
       respond_to do |format|
         if @comment.save
           format.html {
-            flash[:notice] ="Comment was successfully created!"
+            flash[:notice] ="Comment was successfully added!"
             redirect_to :controller=>"comment",:action => "index"
           }
           format.json { render json: @comment, status: :created, location: @comment }
--- app/models/user_comment.rb
+++ app/models/user_comment.rb
@@ -1,5 +1,6 @@
 class UserComment < ActiveRecord::Base
   has_many :user_replies,:dependent => :destroy
-  attr_accessible :comment, :email, :name
+  attr_accessible :comment, :email, :name,:created_at
   validates :name,:email,:comment,:presence => true
+  scope :view, select("name,email,comment,created_at").order("created_at DESC")
 end
