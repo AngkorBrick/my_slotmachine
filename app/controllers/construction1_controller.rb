1e523f4d8794682022d7a2c79962c5a5340aac46
kuroun <sk_roun@yahoo.com>
2014-04-10 10:51:08 +0700

    Implement Navigation in Jquery Mobile

--- app/controllers/construction1_controller.rb
+++ app/controllers/construction1_controller.rb
@@ -1,5 +1,8 @@
 class Construction1Controller < ApplicationController
   def index
     add_breadcrumb "Construction Part 1", :construction1_index_path,:title => "Back to Construction Part 1 Page"
+    @bc="Construction Part 1"
+    @previous=root_path
+    @next=construction2_index_path
   end
 end
--- app/controllers/top_page_controller.rb
+++ app/controllers/top_page_controller.rb
@@ -1,5 +1,8 @@
 class TopPageController < ApplicationController
   def index
     add_breadcrumb "Basic Information", :top_page_index_path,:title => "Back to Basic Info Page"
+    @bc="Basic Information"
+    @previous=comment_index_path
+    @next=construction1_index_path
   end
 end
